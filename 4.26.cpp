0534908171538350f983b28e2f8fb139d2c448c7
MuqiuWhite <1219214097@qq.com>
2021-04-26 21:32:36 +0800

    4.26

---
+++ 4.26.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int k=1,s=0,i;
+	for(i=1;i<=10;i++)
+	{	
+		k=k*i;
+		s=s+k;
+	}
+	printf("%d",s);
+
+	int k=1
+	for(i;i<=100;i++)
+	{
+		s=s+k*k;
+		
+	}
+	
+
+return 0;
+
+ } 
