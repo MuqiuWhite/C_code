52421e09ae9b7d1aa2cfa2873d9528a0cccfb71f
MuqiuWhite <1219214097@qq.com>
2021-05-02 21:49:22 +0800

    C

---
+++ 练习.cpp
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+//	int year,2a;
+//	scanf("%d",&year);
+//	a=year%12;
+//	switch(a)
+//	{
+//		case 0:
+//			printf("猴");			
+//			break;
+//		case 1:
+//			printf("鸡");			
+//			break;
+//		case 2:
+//			printf("狗");			
+//			break;
+//		case 3:
+//			printf("猪");			
+//			break; 
+//		case 4:
+//			printf("鼠");			
+//			break;
+//		case 5:
+//			printf("牛");			
+//			break;
+//		case 6:
+//			printf("虎");			
+//			break;
+//		case 7:
+//			printf("兔");			
+//			break;
+//		case 8:
+//			printf("龙");			
+//			break;
+//		case 9:
+//			printf("蛇");			
+//			break;
+//		case 10:
+//			printf("马");			
+//			break;
+//		case 11:
+//			printf("羊");			
+//			break;
+//	}
+
+//	int score;
+//	scanf("%d",&score);
+//	if(score>=0&&score<=120)
+//	{
+//		switch(score/10)
+//		{	case 12:
+//			case 11:
+//			case 10: 
+//			case 9:
+//				printf("优良");break;
+//			case 8:
+//				printf("良好");break;
+//			case 7:
+//				printf("中等");break;
+//			case 6:
+//				printf("及格");break;
+//			default:
+//				printf("不及格");
+//				break;
+//			 
+//		} 
+//	}
+	
+//	float c=0,f=0;
+//	scanf("%f",&f);
+//	c=(5.0/9.0)*(f-32.0);
+//	printf("%f",c);
+
+//	int a=1,b=2,c=3;
+//	a=b+c++;
+//	c=2*b-c;
+//	b=a++ +b;
+//	printf("a=%d,b=%d,c=%d\n",a,b,c);
+
+//	int n;
+//	printf("please input the score");
+//	scanf("d",&n);
+//	if(n<60)
+//		printf("不及格\n");
+//	else
+//		printf("及格\n");
+
+//	int n;
+//	printf("please input the score	");
+//	scanf("%d",&n);
+//	if(n>=0&&n<=100)
+//	{
+//		if(n<60)
+//			printf("不及格\n");
+//		else
+//			printf("及格\n"); 
+//	}
+//	else
+//		printf("错误！");
+
+//	int n=0,m=3,x=5;
+//	if(!n)x-=1;
+//	if(m) x-=2;
+//	if(x) x-=3;
+//	printf("%d",x);
+
+
+	
+
+
+
+
+
+
+return 0; 
+} 
