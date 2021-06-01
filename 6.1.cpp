5a75754f697849ff288dbe1c53471c5753e6949b
MuqiuWhite <1219214097@qq.com>
2021-06-01 14:23:39 +0800

    C

---
+++ 6.1.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+//void c(int num)
+//{
+//	num+=num;
+//	
+//	return(num);
+//}
+
+//int wdw(int a)
+//{
+//	if(a>9)
+//	{
+//		wdw(a/10);
+//	}
+//	printf("%d ",a%10);
+//}
+
+//void c(n)
+//{
+//	n=n-1;
+//	a=b;
+//	b=c;
+//	c=a+b;
+//	
+//}
+int main()
+{
+//	int i,j;
+//	for(i=101;i<200;i++)
+//	{
+//		for(j=2;j<=sqrt(i);j++)
+//		{
+//			if(i%j==0)
+//			{
+//				break;
+//			}
+//		}
+//		
+//		if(j>sqrt(i))
+//		{
+//			printf("%d  ",i);
+//		}
+//	}
+
+//	int a[5]={0,1,2,3,4};
+//	int l=0;
+//	int r=4;
+//	int n=3;
+//	for(i=0;i<5;i++)
+//	{
+//		if(a[i]<n)
+//		l=m+1
+//	}
+
+//	int num;
+//	c();
+
+//	int a=980924;
+//	wdw(a);
+//	printf("");
+
+	
+return 0;
+ } 
