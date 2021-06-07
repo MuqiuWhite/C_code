c560296bc8748b7678ee20be571347cdbd3bfb10
MuqiuWhite <1219214097@qq.com>
2021-06-07 12:54:29 +0800

    C 快考试了！

---
+++ 6.6.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int x=03,y=02,z=01;
+	int s=x|y&z;
+	printf("%d ",s);
+	printf("%d ",x|y);
+	printf("%d ",y&z);
+	printf("%d ",y&z|x);
+	
+	
+	
+	
+return 0;
+ } 
