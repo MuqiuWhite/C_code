88c00e36eb53aae1b867bd642be45d8ef433ff36
MuqiuWhite <1219214097@qq.com>
2021-06-11 21:47:50 +0800

    c快考试了！

---
+++ 6.11.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+//	char a;
+//	scanf("%d",&a);
+//	if(a>=90)
+//		a='A';
+//	else if(a>=80)
+//		a='B';
+//	else if(a>=70)
+//		a='C';
+//	else if(a>=60)
+//		a='D';
+//	else
+//		a='E';
+//	
+//	printf("%c",a); 
+
+
+	return 0;
+ } 
