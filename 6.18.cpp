212f4743cf0c5fc2b89bbd5da9de28a17b5b5ad1
MuqiuWhite <1219214097@qq.com>
2021-06-19 17:17:33 +0800

    C刷题+日常

--- 6.18.cpp
+++ 6.18.cpp
@@ -1,22 +1,95 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-	int a=0,b=0,c=0,s=0,i=0;
-	while(scanf("%d%d",&a,&b)!=EOF)
+	long n,m;
+	double s=0;
+	int i;
+//	int b=0;
+//	double a;
+	while(scanf("%d%d",&n,&m)!=EOF)
 	{
-		s=0;c=0;
-		for(i=a;i<=b;i++)
+
+
+		int i=0;
+		s=0;
+		for(i=0;i<n;i++)
 		{
-			
-			if(i%2==0)
-			{
-				s=s+i*i;		
-			}
-			else
-				c=c+i*i*i;
-		}		
-		printf("%d  %d\n",s,c);		
+		if(n>0&&n<=10000)
+		{
+			if(i==0)
+				s=n;
+						
+			s=s+sqrt(n);
+			n=sqrt(n);
+		}
+		}
+		printf("%.2lf\n",s);
+	
 	}
 	
 return 0;
- } 
+}
+ 
+ 
+ //	int a=0,b=0,c=0,s=0,i=0,t=0;
+//	while(scanf("%d%d",&a,&b)!=EOF)
+//	{
+//		if(a>b)
+//		{
+//			t=a;
+//			a=b;
+//			b=t;
+//		}
+//		s=0;c=0;
+//		for(i=a;i<=b;i++)
+//		{
+//			
+//			if(i%2==0)
+//			{
+//				s=s+i*i;		
+//			}
+//			else
+//				c=c+i*i*i;
+//		}		
+//		printf("%d  %d\n",s,c);		
+//	} 
+
+
+
+//	int a;
+//	int i=0,j=0,k=0;
+//	double b;
+//	while(scanf("%d",&a)!=EOF&&a)
+//	{
+//		i=0;j=0;k=0;
+//		while(a--)
+//		{
+//			scanf("%lf",&b);
+//	
+//			if(b<0)
+//			{
+//				i++;
+//			}
+//			else if(b==0)
+//				j++;
+//			else 
+//				k++;
+//			
+//		}
+//		printf("%d %d %d\n",i,j,k);
+//	}
+
+
+//		a=0;
+//		for(i=0;i<m;i++)
+//		{
+//			if(i==0)
+//			s[i]=n;
+//			else
+//			{
+//				s[i]=sqrt(s[i-1]);
+//	
+//			}
+//			a+=s[i];
+//		}	
---
+++ 6.19.cpp
@@ -0,0 +1,143 @@
+#include  <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+int main()
+{
+//	int n=0,j=0;
+//	for(n=1;n<100;n++)
+//	{
+//		for(j=2;j<n;j++)
+//		{
+//			if(n%j==0)
+//				break;
+//		}
+//		if(j==n)
+//			printf("%d  ",n);
+//	}
+	
+//	int n=0;
+//	for(n=100;n<1000;n++)
+//	{
+//		int g=n%10;
+//		int s=n/10%10;
+//		int b=n/100;
+//		if(n==g*g*g+s*s*s+b*b*b)
+//		{
+//			printf("%d  ",n);
+//		}
+//	}
+
+//	int a[2][3];
+//	int i,j;
+//	for(i=0;i<2;i++)
+//	{
+//		for(j=0;j<3;j++)
+//		{
+//			scanf("%d",&a[i][j]);
+//		}
+//	}
+//	for(i=0;i<2;i++)
+//	{
+//		for(j=0;j<3;j++)
+//		{
+//			printf("%d ",a[i][j]);
+//		}
+//		printf("\n");
+//	}
+	
+//	int a[20]={1,1};
+//	int n=0,i;
+//	for(i=2;i<20;i++)
+//	{
+//		a[i]=a[i-1]+a[i-2];
+//	}
+//	for(i=0;i<20;i++)
+//	{
+//		printf("%d ",a[i]);
+//		n++;
+//		if(n%5==0)
+//			printf("\n");
+//	}
+
+//	int i;
+//	
+//	double t=0,s=0,a=0;
+//	for(i=1;i<=3;i++)
+//	{
+//		t=t+i;
+//		a=1.0/t;
+//		s=s+a;
+//	}
+//	printf("%f",s);
+	
+10.	
+	int i=1;
+	
+	int t=0,s=0;
+	for(i=1;i<=n;i++)
+	{
+		t=t*i;		
+		s=s+a;
+	}
+	printf("%f",s);
+	return 0;
+}
+
+//	int n,s=1,i=0,t=0,j;
+//	scanf("%d",&n);
+//	for(i=1;i<=n;i++)
+//	{
+//			s=s*i;
+//			t=t+s;	
+//	}
+//	printf("%d ",t);
+
+
+//	int a[10]={1,2,3,4,5,6,7,8,9,10};
+//	int l=0;
+//	int r=9;
+//	int key=7; 
+//	int mid;
+//	while(l<=r)
+//	{
+//		mid=(l+r)/2;
+//			if(key>a[mid])
+//			{
+//				l=mid+1;
+//				
+//			}
+//			else if(key<a[mid])
+//			{
+//				r=mid-1;
+//			}
+//			else
+//			{
+//				printf("%d\n",mid);
+//				break;	
+//			}
+//			
+//		
+//
+//	}
+//		if(l>r)
+//		{
+//			printf("’“≤ªµΩ\n"); 
+//		}
+
+//	char a[]="hello word!\n";
+//	char b[]="***********\n";
+//	int sz=strlen(a);
+//	int l=0;
+//	int r=sz-1;
+//	puts(b);
+//	while(l<=r)
+//	{
+//		b[l]=a[l];
+//		b[r]=a[r];
+//		Sleep(1000);
+//		system("cls");
+//		l++;
+//		r--;
+//		puts(b);
+//	}
