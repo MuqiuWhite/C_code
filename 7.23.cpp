c132327484ff7ec9803bbdab1f3b8459d7153660
MuqiuWhite <1219214097@qq.com>
2021-07-23 03:04:42 +0800

    c

---
+++ 7.23.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int in = 0;
+	printf("Ï²»¶±à³ÌÂğ£¿\n0.²»Ï²»¶¡£1.Ï²»¶\n");
+	scanf("%d", &in);
+	if (in == 0)
+	{
+		printf("ÎØÎØ\n");
+	}
+	else
+	{
+		printf("¼ÓÓÍ£¡\n");
+	}
+	return 0;
+}
