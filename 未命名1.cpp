169d64528cf9f06f850f427d1bb9645666e63e73
MuqiuWhite <1219214097@qq.com>
2021-07-28 15:35:09 +0800

    C

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,63 +1,13 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> 
 int main()
 {
-	int a,b;
-	float s=0;
-	char c;
-	int t;
-	scanf("%d",&t);
-	
-	while(t--);
+	int a[2]={1,2};
+	int b[5]={3};
+	memcpy(b,a,sizeof(int)*k)
+	for(i=0;i<5;i++)
 	{
-		getchar();
-		scanf("%c",&c);
-		scanf("%d%d",&a,&b);
-		switch(c)
-		{
-			case '+':
-				s=a+b;
-				break;
-			case '-':
-				s=a-b;
-				break;
-			case '*':
-				s=a*b;
-				break;
-			case '/':
-				s=(double)a/b;
-				break;
-		}
 	
-		if(c=='/'&&a%b!=0)
-		{
-			printf("%.2f\n",s);
-		}
-		else
-			printf("%d\n",(int)s);
+	printf("%d")
 	}
-
 	return 0;
 }
-
-//int size=0;
-//	int s=0;
-//	int i=0;
-//	char a[300];
-//	while(1)
-//	{
-//		s=0;
-//		gets(a);
-//		if(a[0]=='#')
-//			break;
-//		size=strlen(a);
-//		for(i=0;i<size;i++)
-//		{
-//		
-//			if(a[i]==' ')
-//				continue;
-//			s=s+(i+1)*(a[i]-64);
-//						
-//		}		
-//		printf("%d\n",s);
-//	}
