cb1a8c3e124f9f9376413d813bd2983e3ac73cdd
MuqiuWhite <1219214097@qq.com>
2021-09-05 21:58:49 +0800

    C

---
+++ 9.5/9.5/源.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <assert.h>
+//#include <stdlib.h>
+//int my_strlen(const char* str)
+//{
+//	int c = 0;
+//	while (*str)
+//	{
+//		c++;
+//		str++;
+//	}
+//	return c;
+//}
+// char *my_strcpy(char* a, const char* b)
+//{
+//	char *t = a;
+//	assert(a && b);
+//	while (*t++ = *b++)
+//		;
+//	return (a);
+//}
+// //    char b[] = "abc";
+//	char a[] = "xxxxxxxx";
+//	char* s=my_strcpy(a,b);
+//	printf("%s", a);
+int main()
+{
+
+	return 0;
+}
