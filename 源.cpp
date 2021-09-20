61702ac4a2854dbb1c6d7576e057f41b91a60656
MuqiuWhite <1219214097@qq.com>
2021-09-20 23:54:26 +0800

    C

--- 源.cpp
+++ 源.cpp
@@ -1,21 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h> 
-#include <string.h> 
+#include <stdio.h>
+void* my_memmove(void* a, const void* b,int sz )
+{
+	void* ret = a;
+	if (a < b)//从前向后
+	{
+		while (sz--)
+		{
+			*(char*)a = *(char*)b;
+			a= (char*)a+1;
+			b= (char*)b+1;
+		}
+	}
+	else//从后向前
+	{
+		while (sz--)
+		{
+			*((char*)a+sz) = *((char*)b+sz);
+		}
+	}
+	return a;
+}
 int main()
 {
-	//int a[2] = { 1,2 };
-	//int b[5] = { 3 };
-	//int i;
-//	memcpy(b, a, sizeof(int) * 3);
-	int n = 0;
-	char s[20];
-	while (scanf("%s", s))
+	int a[] = { 1,2,3,4,5,6,7,8,9,10 };
+	
+	my_memmove(a+2,a,20);
+	for (int i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 5; i++)
-		{
-
-			printf("%d", b[i]);
-		}
+		printf("%d  ", a[i]);
 	}
 	return 0;
 }
