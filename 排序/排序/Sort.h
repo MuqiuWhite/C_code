8fedf6c663bc0c61dccd5f2e4545bdbc8e3fb689
MuqiuWhite <1219214097@qq.com>
2021-12-20 23:35:32 +0800

    排序

---
+++ 排序/排序/Sort.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// ≤Â»Î≈≈–Ú
+void InsertSort(int* a, int n);
+// œ£∂˚≈≈–Ú
+void ShellSort(int* a, int n);
+// —°‘Ò≈≈–Ú
+void SelectSort(int* a, int n);
